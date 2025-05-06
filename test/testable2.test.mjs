f5cdbaf1212f2f3321423e73eb743182818e6abd
王昱 <wangyu2021@foxmail.com>
2025-05-06 13:10:29 +0800

    implement test when generator return 0.99

--- test/testable2.test.mjs
+++ test/testable2.test.mjs
@@ -9,5 +9,11 @@ describe("testable 2: a dice game", () => {
       const result = diceRoll(randomGenerator);
       expect(result).to.equal(1);
     });
+
+    test("return 6 when randomGenerator returns 0.99", () => {
+      const randomGenerator = () => 0.99;
+      const result = diceRoll(randomGenerator);
+      expect(result).to.equal(6);
+    });
   });
 });
